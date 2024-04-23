f770b502bdc253e5b389075e25048a54ececc20e
Nick <mykolakolotilo@gmail.com>
2024-04-22 20:28:19 -0600

    feat[portals] practice portals

--- src/components/timer/componnets/ResultModal/ResultModal.jsx
+++ src/components/timer/componnets/ResultModal/ResultModal.jsx
@@ -1,11 +1,12 @@
 import {forwardRef} from "react";
+import {createPortal} from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, onReset}, ref) {
         const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
         const userLost = remainingTime <= 0;
         const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-        return (
+        return createPortal(
             <dialog ref={ref} className="result-modal">
                 {userLost && <h2>You lost</h2>}
                 {!userLost && <h2>Your Score {score}</h2>}
@@ -14,7 +15,8 @@ const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, 
                 <form method='dialog' onSubmit={onReset}>
                     <button>Close</button>
                 </form>
-            </dialog>
+            </dialog>,
+            document.getElementById('modal')
         )
     }
 )
